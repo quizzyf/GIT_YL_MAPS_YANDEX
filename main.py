df6f5b74fc95d690d8a1b5dc0f70da195442188d
quizzy <148488867+antonyy36@users.noreply.github.com>
2024-02-08 20:52:30 +0300

    FIFTH_EXRC

--- main.py
+++ main.py
@@ -5,6 +5,72 @@ import pygame
 import requests
 
 
+class Word:
+    def __init__(self, x, y, width, height, scr):
+        self.x, self.y = x, y
+        self.width = width
+        self.height = height
+        self.text = ''
+        self.scr = scr
+        self.font = pygame.font.SysFont('arial', 20)
+
+    def update(self, i):
+        self.text += i
+
+    def draw(self):
+        txt_surface = self.font.render(str(self.text), True, (0, 0, 0))
+        pygame.draw.rect(self.scr, 'black', (self.x, self.y, self.width, self.height), 1)
+        self.scr.blit(txt_surface, (self.x, self.y))
+
+
+class Button:
+    def __init__(self, x, y, width, height, scr):
+        self.x, self.y = x, y
+        self.width = width
+        self.height = height
+        self.text = 'Искать'
+        self.scr = scr
+        self.font = pygame.font.SysFont('arial', 20)
+        self.buttonRect = pygame.Rect(x, y, self.width, self.height)
+
+    def update(self, pos):
+        if self.buttonRect.collidepoint(pos):
+            return True
+        return False
+
+    def draw(self):
+        txt_surface = self.font.render(str(self.text), True, (0, 0, 0))
+        pygame.draw.rect(self.scr, 'orange', (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(self.scr, 'black', (self.x, self.y, self.width, self.height), 1)
+        self.scr.blit(txt_surface, (self.x + 20, self.y + 2))
+
+
+def geocode(address):
+    # Собираем запрос для геокодера.
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": address,
+        "format": "json"}
+
+    # Выполняем запрос.
+    response = requests.get(geocoder_request, params=geocoder_params)
+
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+    else:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {geocoder_request}
+            Http статус: {response.status_code} ({response.reason})""")
+
+    # Получаем первый топоним из ответа геокодера.
+    # Согласно описанию ответа он находится по следующему пути:
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+
 def show_map(ll_spn=None, map_type='map', add_params=None):
     if ll_spn:
         map_request = f'http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}'
@@ -30,36 +96,64 @@ def show_map(ll_spn=None, map_type='map', add_params=None):
     return map_file
 
 
-def get_coordinates():
-    return float(50.643918), float(55.372823)
+def get_coordinates(address):
+    toponym = geocode(address)
+    if not toponym:
+        return False
+
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return float(toponym_longitude), float(toponym_lattitude)
 
 
 def main():
     z = 10
-    toponym_to_find = get_coordinates()
-    lat, lng = toponym_to_find
-    ll_spn = f"ll={lat},{lng}&z={z}"
-    m_f = show_map(ll_spn, 'map')
+    img = None
     pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    img = pygame.image.load(m_f).convert()
+    screen = pygame.display.set_mode((800, 650))
+    word = Word(150, 600, 300, 30, screen)
+    butn = Button(10, 600, 100, 30, screen)
+    f_poisk = False
     f = True
     while f:
+        if f_poisk:
+            print('dfsdg')
+            toponym_to_find = get_coordinates(word.text)
+            if not toponym_to_find:
+                f_poisk = False
+            else:
+                lat, lng = toponym_to_find
+                ll_spn = f"ll={lat},{lng}&z={z}&pt={lat},{lng}"
+                m_f = show_map(ll_spn, 'map')
+                img = pygame.image.load(m_f).convert()
+                f_poisk = False
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 f = False
             elif i.type == pygame.KEYDOWN:
-                if i.key == pygame.K_PAGEUP and z < 17:
+                if i.key == pygame.K_PAGEUP and z < 17 and img:
                     z += 1
-                    ll_spn = f"ll={lat},{lng}&z={z}"
+                    ll_spn = f"ll={lat},{lng}&z={z}&pt={lat},{lng}"
                     m_f = show_map(ll_spn, 'map')
                     img = pygame.image.load(m_f).convert()
-                elif i.key == pygame.K_PAGEDOWN and z > 0:
+                elif i.key == pygame.K_PAGEDOWN and z > 0 and img:
                     z -= 1
-                    ll_spn = f"ll={lat},{lng}&z={z}"
+                    ll_spn = f"ll={lat},{lng}&z={z}&pt={lat},{lng}"
                     m_f = show_map(ll_spn, 'map')
                     img = pygame.image.load(m_f).convert()
-        screen.blit(img, (0, 0))
+                elif i.key == pygame.K_BACKSPACE:
+                    word.text = word.text[:-1]
+                else:
+                    if i.unicode.lower() in 'йцукенгшщзхъфывапролджэячсмитьбю ,.1234567890':
+                        word.update(i.unicode)
+            elif i.type == pygame.MOUSEBUTTONDOWN:
+                if i.button == 1:
+                    f_poisk = butn.update(i.pos)
+        screen.fill('white')
+        word.draw()
+        butn.draw()
+        if img:
+            screen.blit(img, (0, 0))
         pygame.display.update()
     os.remove(m_f)
     pygame.quit()
