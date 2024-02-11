02f6b73d6e46699ca7ee466bfc1cf845f8941426
quizzy <148488867+antonyy36@users.noreply.github.com>
2024-02-11 15:37:40 +0300

    NINTH EXRC

--- main.py
+++ main.py
@@ -45,6 +45,30 @@ class Button:
         self.scr.blit(txt_surface, (self.x + 25, self.y + 5))
 
 
+class Switch:
+    def __init__(self, x, y, width, height, scr):
+        self.x, self.y = x, y
+        self.width = width
+        self.height = height
+        self.scr = scr
+        self.k = 0
+        self.slov = {1: 'green', 0: 'gray'}
+        self.buttonRect = pygame.Rect(x, y, self.width, self.height)
+
+    def update(self, pos):
+        if self.buttonRect.collidepoint(pos):
+            self.k += 1
+            self.k %= 2
+        if self.k:
+            return True
+        return False
+
+    def draw(self):
+        pygame.draw.rect(self.scr, self.slov[self.k], (self.x, self.y, self.width, self.height), border_radius=20)
+        pygame.draw.ellipse(self.scr, 'white',
+                            (self.x + self.width // 2 * self.k + 2, self.y + 2, self.width // 2 - 4, self.height - 4))
+
+
 def geocode(address):
     # Собираем запрос для геокодера.
     geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
@@ -105,6 +129,17 @@ def get_address(address):
     return toponym_address
 
 
+def get_post_code(address):
+    toponym = geocode(address)
+    if not toponym:
+        return False
+    try:
+        toponym_address_p = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]['postal_code']
+    except KeyError:
+        return ''
+    return ' ' + toponym_address_p
+
+
 def get_coordinates(address):
     toponym = geocode(address)
     if not toponym:
@@ -119,31 +154,22 @@ def main():
     z = 10
     img = None
     adress = ""
-
+    map_type = ['map', 'sat', 'skl']
+    k = 0
     pygame.init()
     font = pygame.font.Font(None, 20)
     screen = pygame.display.set_mode((800, 650))
     word = Word(150, 600, 350, 30, screen)
     butn = Button(10, 600, 100, 30, screen, 'Искать')
     butn_2 = Button(520, 600, 220, 30, screen, 'Сброс поискового результата')
+    switch = Switch(10, 540, 90, 45, screen)
     metka = ''
     string_rendered = font.render(adress, 1, pygame.Color('black'))
-    f_poisk = False
     adress = ""
+    m_f = None
+    post_code = ''
     f = True
     while f:
-        if f_poisk:
-            if word.text:
-                toponym_to_find = get_coordinates(word.text)
-                if toponym_to_find:
-                    lat, lng = toponym_to_find
-                    x_m, y_m = lat, lng
-                    metka = f"&pt={x_m},{y_m}"
-                    ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    adress = get_address(word.text)
-                    m_f = show_map(ll_spn, 'map')
-                    img = pygame.image.load(m_f).convert()
-            f_poisk = False
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 f = False
@@ -151,12 +177,12 @@ def main():
                 if i.key == pygame.K_PAGEUP and z < 17 and img:
                     z += 1
                     ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    m_f = show_map(ll_spn, 'map')
+                    m_f = show_map(ll_spn, map_type[k % 3])
                     img = pygame.image.load(m_f).convert()
                 elif i.key == pygame.K_PAGEDOWN and z > 0 and img:
                     z -= 1
                     ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    m_f = show_map(ll_spn, 'map')
+                    m_f = show_map(ll_spn, map_type[k % 3])
                     img = pygame.image.load(m_f).convert()
                 elif i.key == pygame.K_BACKSPACE:
                     word.text = word.text[:-1]
@@ -167,53 +193,63 @@ def main():
                 if i.key == pygame.K_UP and img:
                     lng += 0.01
                     ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    m_f = show_map(ll_spn, 'map')
+                    m_f = show_map(ll_spn, map_type[k % 3])
                     img = pygame.image.load(m_f).convert()
                 elif i.key == pygame.K_DOWN and img:
                     lng -= 0.01
                     ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    m_f = show_map(ll_spn, 'map')
+                    m_f = show_map(ll_spn, map_type[k % 3])
                     img = pygame.image.load(m_f).convert()
                 elif i.key == pygame.K_LEFT and img:
                     lat -= 0.01
                     ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    m_f = show_map(ll_spn, 'map')
+                    m_f = show_map(ll_spn, map_type[k % 3])
                     img = pygame.image.load(m_f).convert()
                 elif i.key == pygame.K_RIGHT and img:
                     lat += 0.01
                     ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                    m_f = show_map(ll_spn, 'map')
+                    m_f = show_map(ll_spn, map_type[k % 3])
+                    img = pygame.image.load(m_f).convert()
+                elif i.key == pygame.K_EQUALS and img:
+                    k += 1
+                    ll_spn = f"ll={lat},{lng}&z={z}" + metka
+                    adress = get_address(word.text)
+                    m_f = show_map(ll_spn, map_type[k % 3])
                     img = pygame.image.load(m_f).convert()
             elif i.type == pygame.MOUSEBUTTONDOWN:
                 if i.button == 1:
-                    f_poisk = butn.update(i.pos)
-                    toponym_to_find = get_coordinates(word.text)
-                    if toponym_to_find:
-                        lat, lng = toponym_to_find
-                        x_m, y_m = lat, lng
-                        metka = f"&pt={x_m},{y_m}"
-                        ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                        adress = get_address(word.text)
-                        m_f = show_map(ll_spn, 'map')
-                        img = pygame.image.load(m_f).convert()
+                    if butn.update(i.pos):
+                        toponym_to_find = get_coordinates(word.text)
+                        if toponym_to_find:
+                            lat, lng = toponym_to_find
+                            x_m, y_m = lat, lng
+                            metka = f"&pt={x_m},{y_m}"
+                            ll_spn = f"ll={lat},{lng}&z={z}" + metka
+                            adress = get_address(word.text)
+                            m_f = show_map(ll_spn, map_type[k % 3])
+                            img = pygame.image.load(m_f).convert()
                     if butn_2.update(i.pos):
                         metka = ''
                         adress = ''
                         ll_spn = f"ll={lat},{lng}&z={z}" + metka
-                        m_f = show_map(ll_spn, 'map')
+                        m_f = show_map(ll_spn, map_type[k % 3])
                         img = pygame.image.load(m_f).convert()
-
-                    string_rendered = font.render(adress, 1, pygame.Color('black'))
+                    if switch.update(i.pos) and adress:
+                        post_code = get_post_code(word.text)
+                    else:
+                        post_code = ''
+                    string_rendered = font.render(adress + post_code, 1, pygame.Color('black'))
 
         screen.fill('white')
         screen.blit(string_rendered, (0, 500))
         word.draw()
         butn.draw()
         butn_2.draw()
+        switch.draw()
         if img:
             screen.blit(img, (0, 0))
         pygame.display.update()
-    if img:
+    if m_f:
         os.remove(m_f)
     pygame.quit()
 
